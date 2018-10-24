bdb8b45f9597c3777d23fe36d35e77231fc77fb1
Venu <gopalvenu88@outlook.com>
2018-10-24 08:12:24 -0600

    Initial check-in of module AzureWebAppTest

---
+++ AzureWebAppTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureWebAppTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
